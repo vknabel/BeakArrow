b31baa22e7e88b5cd868ec269c1eaf1f9a3e5619
Valentin Knabel <dev@vknabel.com>
2018-11-20 16:55:51 +0100

    Added missing SwiftPM product

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.1.3
+
+* Missing SwiftPM product
+
 ## 0.1.2
 
 * Updated [Beak](https://github.com/yonaskolb/Beak) to 0.4.0
--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "BeakArrow",
+    products: [
+        .executable(
+            name: "BeakArrow",
+            targets: ["BeakArrow"]
+        ),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/vknabel/ArrowKit.git", .branch("master")),
@@ -16,6 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "BeakArrow",
-            dependencies: ["ArrowKit", "BeakCore", "PathKit"]),
+            dependencies: ["ArrowKit", "BeakCore", "PathKit"]
+        ),
     ]
 )
